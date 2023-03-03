03a64b2f115028134ad5331f7b47f5dc18c11b91
Salim Aydın <salimaydin340@gmail.com>
2023-03-03 20:48:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [liste, setListe] = useState([]);
+
+  const handleSubmit = () => {
+    if (name.trim() !== "") { // boşluklar hariç, name değişkeninin içeriği boş değilse
+      setListe([...liste, name]);
+      setName("");
+    }
+  };
+
+  const tetiklenen = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  const handleDelete = (index) => {
+    // 1. Kopyala
+    const newList = [...liste];
+    // 2. Sil
+    newList.splice(index, 1);
+    // 3. Ayarla
+    setListe(newList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div id="formum">
+        <input onChange={tetiklenen} onKeyDown={handleKeyDown} value={name} />
+      <button onClick={handleSubmit}>+</button>
+        </div>
+      <div id="listem">
+      <ul>
+        {liste.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => handleDelete(index)}>-</button>
+          </li>
+        ))}
+      </ul>
+      </div>
+      
     </div>
   );
 }
